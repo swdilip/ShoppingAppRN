95a24c90fff604441be62a1e1f6d032d0e1b17d7
Swastik Dilip <93096031+s-dilip@users.noreply.github.com>
2023-05-02 17:09:16 +0100

    feat: added display of product details on product screen

--- ShoppingAppRN/src/screens/ProductScreen.tsx
+++ ShoppingAppRN/src/screens/ProductScreen.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import {useRoute} from '@react-navigation/native';
 
 export default function ProductScreen() {
   const route = useRoute();
   const product = route.params?.product;
 
-  return product && <Text>{product.title}</Text>;
+  return (
+    product && (
+      <View>
+        <Text>{product.title}</Text>
+        <View>
+          <Image
+            source={{uri: product.image}}
+            style={{width: 150, height: 150}}
+          />
+        </View>
+        <Text>Category: {product.category}</Text>
+        <Text>Description: {product.description}</Text>
+        <Text>
+          Rating: {product.rating.rate} by {product.rating.count} Users
+        </Text>
+      </View>
+    )
+  );
 }
