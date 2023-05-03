6e9b4e55a6dd6a2ab35ca3467bf09fceff1c147f
Swastik Dilip <93096031+s-dilip@users.noreply.github.com>
2023-05-03 13:36:24 +0100

    feat:added remove from cart button

--- ShoppingAppRN/src/screens/ShoppingCartScreen.tsx
+++ ShoppingAppRN/src/screens/ShoppingCartScreen.tsx
@@ -1,17 +1,30 @@
 import React, {useContext} from 'react';
-import {Text, ScrollView} from 'react-native';
+import {Text, ScrollView, Button, View} from 'react-native';
 import {ShopCartContext} from '../context/ShopCartContext';
 import ProductCard from '../components/ProductCard';
 
 export default function ({navigation}) {
   const {items, setItems} = useContext(ShopCartContext);
 
+  function removeItem(itemId) {
+    const updatedItems = items.filter(item => item.id !== itemId);
+    setItems(updatedItems);
+  }
+
   return (
     <ScrollView>
       <Text>Home Screen</Text>
       {items &&
         items.map((item, i) => (
-          <ProductCard product={item} navigation={navigation} key={i} />
+          <View>
+            <ProductCard product={item} navigation={navigation} key={i} />
+            <Button
+              title="Remove from Cart"
+              onPress={() => {
+                removeItem(item.id);
+              }}
+            />
+          </View>
         ))}
     </ScrollView>
   );
