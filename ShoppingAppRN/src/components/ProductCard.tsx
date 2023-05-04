ff888443d927d489e6b631f4dfbb38d190067e18
Swastik Dilip <93096031+s-dilip@users.noreply.github.com>
2023-05-04 12:57:20 +0100

    feat:added image styling for productcard

--- ShoppingAppRN/src/components/ProductCard.tsx
+++ ShoppingAppRN/src/components/ProductCard.tsx
@@ -5,17 +5,15 @@ export default function ProductCard({product, navigation}) {
   return (
     <TouchableHighlight
       onPress={() => navigation.navigate('Product', {productId: product.id})}>
-      {/* <View style={styles.cardShadow}> */}
       <View style={styles.card}>
-        <Text style={styles.title}>{product.title}</Text>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.title}>{product.title}</Text>
+          <Text style={styles.price}>£{product.price}</Text>
+        </View>
         <View style={styles.picture}>
-          <Image
-            source={{uri: product.image}}
-            style={{width: 100, height: 100}}
-          />
+          <Image source={{uri: product.image}} style={styles.displayImg} />
         </View>
       </View>
-      {/* </View> */}
     </TouchableHighlight>
   );
 }
@@ -32,10 +30,21 @@ const styles = StyleSheet.create({
     shadowColor: '#52006A',
     flexDirection: 'row-reverse',
   },
+  detailsContainer: {flex: 0.7},
   title: {
-    flex: 0.7,
+    fontWeight: 'bold',
+    fontSize: 15,
   },
+  price: {fontWeight: 'bold', fontSize: 15, marginTop: 8, color: 'blue'},
   picture: {
     flex: 0.3,
+    marginRight: 15,
+    width: 100,
+    height: 100,
+  },
+  displayImg: {
+    height: 100,
+    width: 100,
+    resizeMode: 'contain',
   },
 });
