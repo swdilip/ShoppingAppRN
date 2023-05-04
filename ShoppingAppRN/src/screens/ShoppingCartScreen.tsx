484e25f3fb135c6874c9dcdcd06dba822f8cb2c7
Swastik Dilip <93096031+s-dilip@users.noreply.github.com>
2023-05-04 17:47:59 +0100

    feat:added a total price calculator

--- ShoppingAppRN/src/screens/ShoppingCartScreen.tsx
+++ ShoppingAppRN/src/screens/ShoppingCartScreen.tsx
@@ -1,32 +1,65 @@
-import React, {useContext, useEffect} from 'react';
-import {Text, ScrollView, Button, View, FlatList} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {Text, Button, View, FlatList, StyleSheet} from 'react-native';
 import {ShopCartContext} from '../context/ShopCartContext';
 import ProductCard from '../components/ProductCard';
 
 export default function ({navigation}) {
   const {items, setItems} = useContext(ShopCartContext);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   function removeItem(itemCartId) {
     const updatedItems = items.filter(item => item.cartId !== itemCartId);
     setItems(updatedItems);
   }
 
+  useEffect(() => {
+    function sum(total, item) {
+      return total + item.product.price;
+    }
+    const total = items?.reduce(sum, 0);
+
+    setTotalPrice(total);
+  }, [items]);
+
   return items.length > 0 ? (
-    <FlatList
-      data={items}
-      renderItem={item => (
-        <View>
-          <ProductCard product={item.item.product} navigation={navigation} />
-          <Button
-            title="Remove from Cart"
-            onPress={() => {
-              removeItem(item.item.cartId);
-            }}
-          />
-        </View>
-      )}
-    />
+    <View>
+      <FlatList
+        data={items}
+        renderItem={item => (
+          <View>
+            <ProductCard product={item.item.product} navigation={navigation} />
+            <Button
+              title="Remove from Cart"
+              onPress={() => {
+                removeItem(item.item.cartId);
+              }}
+            />
+          </View>
+        )}
+      />
+      <Text>Total: {totalPrice}</Text>
+    </View>
   ) : (
-    <Text>No Items in your Cart!</Text>
+    <View style={styles.cartEmptyView}>
+      <Text style={styles.cartEmptyText}>No Items found in your 🛒!</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  cartEmptyView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cartEmptyText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+  },
+  itemDiscardBtn: {
+    flex: 0.2,
+  },
+});
