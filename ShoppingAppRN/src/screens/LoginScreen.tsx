22a6ed0d774d6b077bf46708a923509809c7e6fd
Swastik Dilip <93096031+s-dilip@users.noreply.github.com>
2023-05-15 13:02:02 +0100

    feat:created changehandler for username text input

--- ShoppingAppRN/src/screens/LoginScreen.tsx
+++ ShoppingAppRN/src/screens/LoginScreen.tsx
@@ -1,10 +1,11 @@
 import {Text, Button, View, TextInput} from 'react-native';
 import {UserAuthContext} from '../context/UserAuthContext';
-import React, {useCallback, useContext} from 'react';
+import React, {useCallback, useContext, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 
 export default function LoginScreen() {
   const {login, setUser} = useContext(UserAuthContext);
+  const [userName, setUserName] = useState('');
   const navigation = useNavigation();
 
   const loginUser = async () => {
@@ -13,14 +14,18 @@ export default function LoginScreen() {
     // 3. If the response is 200, store the token in storage AND in context
     const response = {statusCode: 200, data: {token: '123-456-789'}};
     await login(response.data.token);
-    await setUser('dummy');
+    await setUser(userName);
     navigation.reset({routes: [{name: 'App'}]});
   };
+
+  function onChangeHandler(e) {
+    setUserName(e);
+  }
 
   return (
     <View>
       <Text>login</Text>
-      <TextInput placeholder="Username" />
+      <TextInput placeholder="Username" onChangeText={onChangeHandler} />
       <TextInput placeholder="Password" secureTextEntry={true} />
       <Button
         title="Log In"
